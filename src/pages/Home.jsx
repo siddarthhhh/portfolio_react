40aea32c425820c07e6a1bfd799004e459f979d8
siddarthhhh <chidurulasiddarth@gmail.com>
2025-07-22 13:14:25 +0530

    feat: setup basic routing with Home and NotFound pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return <div className='min-h-screen'>
+    
+    {/* theme toggle */}
+
+
+    {/* background effect */}
+
+    {/* navbar */}
+
+    {/* main content */}
+
+    {/* footer */}
+
+  </div>
+}
+
+export default Home
